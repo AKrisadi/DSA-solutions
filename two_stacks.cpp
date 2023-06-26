fed83d5f454d99d784faf5eeff534d27bac1cca2
AKrisadi <106695351+AKrisadi@users.noreply.github.com>
2023-06-26 01:44:14 -0700

    Create two_stacks.cpp

---
+++ two_stacks.cpp
@@ -0,0 +1,56 @@
+// Problem LInk - https://www.codingninjas.com/studio/problems/two-stacks_983634
+#include <bits/stdc++.h> 
+class TwoStack {
+    int size;
+    int *arr;
+    int top1;
+    int top2;
+public:
+
+    // Initialize TwoStack.
+    TwoStack(int s) {
+        // Write your code here.
+        size = s;
+        arr = new int[size];
+        top1 = -1;
+        top2 = size;
+    }
+    
+    // Push in stack 1.
+    void push1(int num) {
+        // Write your code here.
+        if (top2 - top1 > 1){
+            top1++;
+            arr[top1] = num;
+        }
+    }
+
+    // Push in stack 2.
+    void push2(int num) {
+        // Write your code here.
+        if (top2 - top1 > 1){
+            top2--;
+            arr[top2] = num;
+        }
+    }
+    // Pop from stack 1 and return popped element.
+    int pop1() {
+        // Write your code here.
+        if (top1 >= 0){
+            int val = arr[top1];
+            top1--;
+            return val;
+        }
+        else return -1;
+    }
+    // Pop from stack 2 and return popped element.
+    int pop2() {
+        // Write your code here.
+        if (top2 <= size - 1){
+            int val = arr[top2];
+            top2++ ;
+            return val;
+        }
+        else return -1;
+    }
+};
