faa279d2aa6c98ffab3c3c565f5859faae05890d
AKrisadi <106695351+AKrisadi@users.noreply.github.com>
2023-09-21 22:41:33 +0530

    Create merge_point_of_two_linked_lists.cpp

---
+++ merge_point_of_two_linked_lists.cpp
@@ -0,0 +1,47 @@
+int length(Node *head)
+{
+    int length = 0;
+    Node *tmp = head;
+
+    while (tmp != NULL)
+    {
+        tmp = tmp->next;
+        length++;
+    }
+
+    return length;
+}
+
+int findIntersection(Node *firstHead, Node *secondHead)
+{
+
+    //Get the length of both list
+    int firstListLength = length(firstHead), secondListLength = length(secondHead);
+
+    //Move headA and headB to the same start point
+    while (firstListLength > secondListLength)
+    {
+        firstHead = firstHead->next;
+        firstListLength--;
+    }
+
+    while (firstListLength < secondListLength)
+    {
+        secondHead = secondHead->next;
+        secondListLength--;
+    }
+
+    //Find the intersection until end
+    while (firstHead != secondHead)
+    {
+        firstHead = firstHead->next;
+        secondHead = secondHead->next;
+    }
+
+    if (firstHead == NULL)
+    {
+        return -1;
+    }
+
+    return firstHead->data;
+}
