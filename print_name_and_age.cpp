58b323a4044d7bd0e54ed1150d6a3310bd78a5e6
AKrisadi <106695351+AKrisadi@users.noreply.github.com>
2023-11-07 13:23:53 +0530

    Create print_name_and_age.cpp

---
+++ print_name_and_age.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Write your code here
+string name;
+
+        int age;
+
+        cin>>name>>age;
+
+        cout<<"The name of the person is"<<" "<<name<<" "<<"and the age is "<<age<<"."<<endl;
+    return 0;
+}
